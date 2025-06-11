6d614d0324336b68f18f66382a05df1fceea95a7
Jeff Bizo <jeffreydis10@gmail.com>
2025-06-11 11:03:12 +0200

    🔧 DB Setup | PostgreSQL (Neon) + Drizzle ORM
    ✅ Initialisation de la base de données PostgreSQL via Neon.tech
    🧠 Ajout de Drizzle ORM pour la gestion typée des entités
    📐 Création du premier schéma SQL avec Drizzle (tables, types, relations)
    🔍 Vérification de la structure dans Neon (interface web)
    🛠️ Inspection et validation du schéma via Drizzle Studio (drizzle-kit studio)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,9 @@ import { Button } from "@/components/ui/button";
 
 export default function Home() {
   return (
-    <Button  >
-      Hello
-    </Button>
+    <div className="text-3xl">
+      <Button className="text-3xl bg-amber-600">Click me</Button>
+    </div>
+   
   );
 }
