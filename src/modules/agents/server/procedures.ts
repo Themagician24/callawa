042cae1eadd571093586cfa5d9609ab6d2f0a2bd
Jeff Bizo <jeffreydis10@gmail.com>
2025-06-24 08:21:17 +0200

    13- Agent Filters

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,8 +1,16 @@
+import { DEFAULT_PAGE, 
+  DEFAULT_PAGE_SIZE, 
+  Max_PAGE_SIZE,
+   MIN_PAGE_SIZE }
+
+    from '@/constants';
+
+
 import { db } from '@/db';
 import { agents } from '@/db/schema';
 import { agentsInsertSchema } from '@/modules/agents/schema';
 import { createTRPCRouter , protectedProcedure } from '@/trpc/init';
-import { eq, getTableColumns, sql } from 'drizzle-orm';
+import { and, count, desc, eq, getTableColumns, ilike, sql } from 'drizzle-orm';
 import { z } from 'zod';
 
 
@@ -14,7 +22,11 @@ export const agentsRouter = createTRPCRouter({
 
   getOne: protectedProcedure.input(z.object({ id: z.string() })).query(async ({ input }) => {
     const [existingAgent] = await db
-    .select()
+    .select({
+      // TODO: Changer la valeur de meetingCount en valeur actuelle;
+      meetingCount: sql<number>`5`,
+      ...getTableColumns(agents),
+    })
     .from(agents)
     .where(eq(agents.id, input.id))
 
@@ -27,7 +39,32 @@ export const agentsRouter = createTRPCRouter({
 
   //  TODO: Changer getMany to use ProtectedProcedure
 
-  getMany: protectedProcedure.query(async () => {
+  getMany: protectedProcedure
+
+  .input(z.object({
+      page: z
+      .number()
+      .min(1)
+      .default(DEFAULT_PAGE),
+
+      pageSize: 
+      z
+      .number()
+      .min(MIN_PAGE_SIZE)
+      .max(Max_PAGE_SIZE)
+      .default(DEFAULT_PAGE_SIZE),
+
+
+      search: z
+      .string()
+      .nullish(),
+  })
+)
+
+
+
+  .query(async ({ ctx, input  }) => {
+    const { search, page, pageSize } = input;
     const data = await db
     .select({
       ...getTableColumns(agents),
@@ -35,7 +72,37 @@ export const agentsRouter = createTRPCRouter({
       meetingCount: sql<number>`5`,
 
     })
-    .from(agents);
+    .from(agents)
+    .where(
+      and(
+      eq(agents.userId, ctx.auth.user.id),
+      search ? ilike(agents.name, `%${search}%`) : undefined,
+    )
+  )
+      .orderBy(desc(agents.createdAt), desc(agents.id) )
+      .limit(pageSize)
+      .offset((page - 1) * pageSize);
+
+     const total = await db
+     .select({ count: count() })
+     .from(agents)
+     .where(
+       and(
+         eq(agents.userId, ctx.auth.user.id),
+         search ? ilike(agents.name, `%${search}%`) : undefined,
+       )
+     )
+     
+     const totalPages = Math.ceil(total[0].count / pageSize);
+
+
+     return {
+      items: data,
+      total: total[0].count, 
+      totalPages
+     }
+
+
     return data;
   }),
 
--- src/modules/agents/views/agent-views.tsx
+++ src/modules/agents/views/agent-views.tsx
@@ -3,7 +3,9 @@
 
 import { EmptyState } from "@/components/empty-state";
 import { LoadingState } from "@/components/loading-state";
+import { useAgentFilters } from "@/modules/agents/hooks/use-agents-filters";
 import {  columns } from "@/modules/agents/ui/components/columns";
+import { DataPagination } from "@/modules/agents/ui/components/data-pagination";
 import { DataTable } from "@/modules/agents/ui/components/data-table";
 
 
@@ -17,11 +19,18 @@ import { useSuspenseQuery } from "@tanstack/react-query";
 
 
 export const AgentsView = () => {
+
+  const [ filters, setFilters  ] = useAgentFilters();
+
+
+
   // Initialisation du client TRPC
   const trpc = useTRPC();
 
   // Récupération des agents avec react-query
-  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions());
+  const { data } = useSuspenseQuery(trpc.agents.getMany.queryOptions({
+    ...filters,
+  }));
 
  
 
@@ -30,8 +39,17 @@ export const AgentsView = () => {
     <div className="flex-1 pb-4 px-4 md:px-8 flex flex-col gap-y-4">
 
 
-      <DataTable data={data} columns={columns} />
-      {data.length === 0 && (
+      <DataTable data={Array.isArray(data) ? data : data.items} columns={columns} />
+
+      <DataPagination 
+      page={filters.page}
+      totalPages={Array.isArray(data) ? 1 : data.totalPages}
+      onPageChange={(page) => setFilters({ page })}
+      />
+
+
+
+      {(Array.isArray(data) ? data.length === 0 : data.items.length === 0) && (
         <EmptyState
           title="Create your first agent"
           description="Create an agent to join your meetings.Each agent will follow your instructions and can interact with the participants during the call."
